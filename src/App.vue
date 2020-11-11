e2a0c61f9349436d64c8fd4a1aa9caa9dc706209
Maximilian <max.helmersson@gmail.com>
2020-11-11 16:02:34 +0100

    Chapter 1-6

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile";
+
+export default {
+  nam: 'App',
+  components: { UserProfile }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  margin-top: 60px
+  display: flex
+  flex-direction: column
+  text-align: left
+
+</style>
